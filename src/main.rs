8ad095f69a960df6bdc841befadfbf746271742b
swordfish <jacksoncofficial3@gmail.com>
2024-11-04 14:27:14 -0600

    fix help output

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,19 @@ fn commandler() -> Command {
         .about("Track that jaunt")
         .subcommand_required(false)
         .arg_required_else_help(true)
-        .subcommand(Command::new("init").about("Initialize a new jaunt repo"))
+        .subcommand(
+            Command::new("init")
+            .about("Initialize a new jaunt repo")
+        )
         .subcommand(
             Command::new("track")
                 .about("Choose the files that jaunt tracks")
                 .arg_required_else_help(true)
-                .arg(
-                    Arg::new("remove")
-                        .help("Untrack files")
-                        .short('R')
-                        .long("remove"),
-                ),
         )
         .subcommand(
-            Command::new("commit")
-                .about("Update what jaunt knows")
-                .arg_required_else_help(true)
-                .arg(arg!(<MESSAGE> "Commit Message")),
-        )
-        .subcommand(
-            Command::new("shove")
-                .about("Push changes remotely")
-                .arg_required_else_help(true),
+            Command::new("untrack")
+            .about("Stop tracking a file")
+            .arg_required_else_help(true)
         )
 }
 
