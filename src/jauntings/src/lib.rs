800015c6264c32dcb0f7e514bb1f1511ee6bbae9
swordfish <jacksoncofficial3@gmail.com>
2024-11-04 12:29:08 -0600

    stuff

--- src/jauntings/src/lib.rs
+++ src/jauntings/src/lib.rs
@@ -1,14 +1,48 @@
+use std::process::{exit};
+use std::env::{current_dir};
+use std::fs::{create_dir, File};
+
 pub fn jaunt_init(){
- ()
+    match current_dir() {
+        Ok(dir) => {
+            let dot_jaunt = dir.join(".jaunt");
+            let jaunt_dot_toml = dir.join("jaunt.toml");
+
+            if dot_jaunt.exists() || jaunt_dot_toml.exists(){
+                eprintln!("A jaunt repo or its side effects already exists in this directory");
+                exit(0);
+            }else{
+                match create_dir(dir.join(".jaunt").as_path()) {
+                    Ok(()) => (),
+                    Err(e) => {
+                        eprintln!("Error {}", e);
+                        exit(1);
+                    }
+                }
+
+                match File::create_new(dir.join("jaunt.toml").as_path()) {
+                    Ok(new_toml) => {
+                        // TODO: Write boiler plate file contents
+                        // TODO: write data to file
+                        /*
+                        [project]
+                        name = "dir_name"
+                        
+                         */
+                        let file_content = format!("");
+
+                        
+                    },
+                    Err(e) => {
+                        eprintln!("Error {}", e);
+                        exit(1);
+                    }
+                }
+            }
+        },
+        Err(e) => {
+            eprintln!("Error {}", e);
+            exit(1);
+        }
+    }
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
-use clap::{arg, Command, Arg, ArgAction};
+use jauntings::jaunt_init;
+
+use clap::{arg, Arg, ArgAction, Command};
 
 fn commandler() -> Command {
     Command::new("jaunt")
         .about("Track that jaunt")
         .subcommand_required(false)
         .arg_required_else_help(true)
+        .subcommand(Command::new("init").about("Initialize a new jaunt repo"))
         .subcommand(
-            Command::new("init")
-            .about("Initialize a new jaunt repo")
-        ).subcommand(
             Command::new("track")
-            .about("Choose the files that jaunt tracks")
-            .arg_required_else_help(true)
-            .arg(
+                .about("Choose the files that jaunt tracks")
+                .arg_required_else_help(true)
+                .arg(
                     Arg::new("remove")
-                    .help("Untrack files")
-                    .short('R')
-                    .long("remove")
-                )
+                        .help("Untrack files")
+                        .short('R')
+                        .long("remove"),
+                ),
         )
         .subcommand(
             Command::new("commit")
-            .about("Update what jaunt knows")
-            .arg_required_else_help(true)
-            .arg(arg!(<MESSAGE> "Commit Message"))
+                .about("Update what jaunt knows")
+                .arg_required_else_help(true)
+                .arg(arg!(<MESSAGE> "Commit Message")),
         )
         .subcommand(
             Command::new("shove")
-            .about("Push changes remotely")
-            .arg_required_else_help(true)
+                .about("Push changes remotely")
+                .arg_required_else_help(true),
         )
 }
 
@@ -36,8 +36,9 @@ fn main() {
     let matches = commandler().get_matches();
 
     match matches.subcommand() {
-        Some(("init", cmd)) => {
-            
+        Some(("init", _)) => {
+            jaunt_init();
         }
+        _ => {}
     }
 }
