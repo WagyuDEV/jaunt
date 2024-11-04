8902420e20d50ae18f18c117f87d041e606bacec
swordfish <jacksoncofficial3@gmail.com>
2024-11-04 14:13:00 -0600

    remove .jaunt

--- src/jauntings/src/lib.rs
+++ src/jauntings/src/lib.rs
@@ -5,21 +5,12 @@ use std::fs::{create_dir, File};
 pub fn jaunt_init(){
     match current_dir() {
         Ok(dir) => {
-            let dot_jaunt = dir.join(".jaunt");
             let jaunt_dot_toml = dir.join("jaunt.toml");
 
-            if dot_jaunt.exists() || jaunt_dot_toml.exists(){
+            if jaunt_dot_toml.exists(){
                 eprintln!("A jaunt repo or its side effects already exists in this directory");
                 exit(0);
             }else{
-                match create_dir(dir.join(".jaunt").as_path()) {
-                    Ok(()) => (),
-                    Err(e) => {
-                        eprintln!("Error {}", e);
-                        exit(1);
-                    }
-                }
-
                 match File::create_new(dir.join("jaunt.toml").as_path()) {
                     Ok(new_toml) => {
                         // TODO: Write boiler plate file contents
